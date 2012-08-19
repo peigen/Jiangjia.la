2b6d0f1e711200983dac384ffb2be6e3b725ffcf
peigen <peigen123@gmail.com>
2012-08-20 00:10:23 +0800

    修改发布脚本

--- deploy-on-tomcat.sh
+++ deploy-on-tomcat.sh
@@ -1,7 +1,7 @@
 export project_name=depreciate
-export project_path=/Users/peigen/workspace/peigen/${project_name}
+export project_path=/home/peigen/soft/Jiangjia.la
 export war_path=${project_path}/${project_name}-assemble/target/ROOT.war
-export tomcat_path=$MY_JAVA/apache-tomcat-7.0.22
+export tomcat_path=$MY_JAVA/apache-tomcat-7.0.29
 
 echo project name is ${project_name}
 echo project path is ${project_path}
@@ -22,8 +22,8 @@ rm -rf ${tomcat_path}/work/ ;
 
 echo '开始打包';
 cd ${project_path};
-#mvn install -Dmaven.test.skip=true;
-mvninstall_without_test;
+mvn install -Dmaven.test.skip=true;
+#mvninstall_without_test;
 
 echo '开始复制完毕';
 cp -r ${war_path} ${tomcat_path}/webapps/
